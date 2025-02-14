5ea3c66f938d2bc19890a970fb9b8f286040a96a
Fiorella-vq <fio.89@hotmail.com>
2025-02-14 16:14:49 -0300

    Ejercicio 1

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,4 +1,10 @@
 function isOldEnoughToDrink(age) {
     // your code here
-    
+    return age >=21
 }
+
+let output = isOldEnoughToDrink(22);
+console.log(output); // --> true
+
+console.log(isOldEnoughToDrink(18)); 
+console.log(isOldEnoughToDrink(21)); 
